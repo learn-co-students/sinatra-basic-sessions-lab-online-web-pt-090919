7f7227a264f833c55f560294ff694cf3710c568f
Srikalyan Megati <kalmegati@gmail.com>
2019-12-18 17:01:34 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session_secret, "dimir"
+  end
+  
+  get "/" do
+    erb :index
+  end
+  
+  post "/checkout" do
+    params.each { |k,v|
+      session[k.to_s] = v
+    }
+  end
+  
 end
